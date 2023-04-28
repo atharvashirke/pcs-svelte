c00099f309a92777718eeca5d2d4ecde5eaebadf
Atharva Shirke <atharvashirke@berkeley.edu>
2023-04-27 17:38:33 -0700

    sveltekit + tailwind + flowbite + supabase starter finished

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang='ts'>
+    import "../app.css";
+    import { invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
+	import type { LayoutData } from './$types'
+
+	export let data: LayoutData
+
+	$: ({ supabase, session } = data)
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth')
+			}
+		})
+
+		return () => data.subscription.unsubscribe()
+	})
+</script>
+
+<div class="container" style="padding: 50px 0 100px 0">
+	<slot />
+</div>
